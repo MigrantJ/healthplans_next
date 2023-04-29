221de70d121d4652d39ad70d497565631cdb7e3f
Jim Grant <jimgrant@gmail.com>
2023-04-29 20:06:39 +0000

    experimental UI layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,84 @@
 "use client";
-import { Link } from "@chakra-ui/next-js";
+import { useState } from "react";
+import { Box, Flex, Button, Heading, Input } from "@chakra-ui/react";
+
+import PlanList from "@/components/PlanList";
+
+interface Place {
+  zipcode: string;
+  countyfips: string;
+  state: string;
+}
 
 export default function IndexPage() {
+  const [zipCode, setZipCode] = useState("");
+  const [countyCode, setCountyCode] = useState("");
+  const [state, setState] = useState("");
+
+  const getPlaceByLatLong = async (lat: number, long: number) => {
+    const res = await fetch(`/api/location?lat=${lat}&long=${long}`);
+    if (!res.ok) {
+      throw new Error(`Error: ${res.status}`);
+    }
+    const place = (await res.json()) as Place;
+    setZipCode(place.zipcode);
+    setCountyCode(place.countyfips);
+    setState(place.state);
+  };
+
+  const getPosByGPS = function () {
+    const successCallback: PositionCallback = (position) => {
+      void getPlaceByLatLong(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+    };
+
+    const errorCallback: PositionErrorCallback = (error) => {
+      // todo: better error handling
+      console.log(error);
+    };
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  };
+
   return (
-    <div>
-      <Link href={`/getloc`} color="blue.400" _hover={{ color: "blue.500" }}>
-        test get loc
-      </Link>
-    </div>
+    <Box>
+      <Flex h="100vh" gap={1} paddingTop="5px">
+        <Flex
+          direction="column"
+          bg="blue.500"
+          paddingY="10px"
+          paddingX="5px"
+          w="250px"
+          align="center"
+        >
+          <Heading>Setup</Heading>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              getPosByGPS();
+            }}
+          >
+            Use GPS
+          </Button>
+          <Input
+            id="zipcode"
+            value={zipCode}
+            placeholder="Zip Code"
+            onChange={(e) => setZipCode(e.target.value)}
+          />
+        </Flex>
+        <Flex direction="column" gap={1} w="100%">
+          <Flex bg="red.500" h="50px">
+            <Heading>Filters</Heading>
+          </Flex>
+          <Flex bg="green.500" h="100%">
+            <Heading>Data Window</Heading>
+            <PlanList zipCode={zipCode} state={state} countyCode={countyCode} />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
