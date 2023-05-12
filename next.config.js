abb353438587cab669c69801b7cce401dc178ef1
Jim Grant <jimgrant@gmail.com>
2023-05-12 17:56:43 -0500

    added configurable maintenance page redirect with env var

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    redirects: () => {
+        return [
+            process.env.MAINTENANCE_MODE === "1"
+            ? { source: "/((?!maintenance).*)", destination: "/maintenance", permanent: false }
+            : null,
+            ].filter(Boolean);
+    }
 };
 
 module.exports = nextConfig;
