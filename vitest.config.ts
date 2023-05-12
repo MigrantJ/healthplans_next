65f6babad8b9c8a2b77d795e04b3ef91d39d21cb
Jim Grant <jimgrant@gmail.com>
2023-05-12 17:23:47 -0500

    resolving linting errors for vercel deployment

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  plugins: react(),
+  plugins: [react()],
   test: {
     environment: "happy-dom",
   },
