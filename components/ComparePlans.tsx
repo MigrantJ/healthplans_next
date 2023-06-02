c61dc8685c989eaea539ac2764a8e22445945123
Jim Grant <jimgrant@gmail.com>
2023-06-02 15:34:44 -0500

    POC of collapsible areas for ComparePlans

--- components/ComparePlans.tsx
+++ components/ComparePlans.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Grid, GridItem, Box, Text, Heading } from "@chakra-ui/react";
 import {
   Provider,
@@ -13,6 +14,8 @@ interface IProps {
 }
 
 export default function ComparePlans({ plans }: IProps) {
+  const [expand, setExpand] = useState(true);
+
   return (
     <Provider>
       <Grid gridTemplateColumns="50px 500px 50px" margin="0 auto">
@@ -20,40 +23,64 @@ export default function ComparePlans({ plans }: IProps) {
         <Grid
           gridColumn="2/3"
           gridRow="1/2"
-          gridTemplateRows="repeat(4, 40px 50px)"
+          gridTemplateRows="40px repeat(3, 40px 50px) 40px repeat(3, 40px 50px)"
+          paddingTop="4px"
+          alignItems="center"
         >
-          <GridItem padding="5px" backgroundColor="lightgray">
-            <Heading size="md">TestComparePlans</Heading>
+          <GridItem backgroundColor="lightgray" height="100%">
+            <Heading size="md">Costs</Heading>
           </GridItem>
+          <Box display={expand ? "contents" : "none"}>
+            <Heading size="sm">Estimated Monthly Premium</Heading>
+            <GridItem />
+            <Heading size="sm">Deductible</Heading>
+            <GridItem />
+            <Heading size="sm">Out-of-Pocket Maximum</Heading>
+            <GridItem />
+          </Box>
+
+          <GridItem backgroundColor="lightgray" height="100%">
+            <Heading size="md">Misc</Heading>
+          </GridItem>
+          <Heading size="sm">Metal Level</Heading>
           <GridItem />
-          <Heading size="md">Estimated Monthly Premium</Heading>
+          <Heading size="sm">Plan Type</Heading>
           <GridItem />
-          <Heading size="md">Deductible</Heading>
-          <GridItem />
-          <Heading size="md">Out-of-Pocket Maximum</Heading>
+          <Heading size="sm">Plan ID</Heading>
           <GridItem />
         </Grid>
         <Box gridColumn="2/3" gridRow="1/2">
-          <Carousel gap={50}>
+          <Carousel gap={0}>
             {plans.map((plan, i) => {
               return (
                 <Grid
                   key={plan.id}
-                  gridTemplateRows="repeat(4, 40px 50px)"
+                  gridTemplateRows="40px repeat(3, 40px 50px) 40px repeat(3, 40px 50px)"
                   justifyItems="center"
                   alignItems="center"
                   borderInline="1px solid lightgray"
                   minWidth="225px"
-                  paddingInline="10px"
                 >
+                  <GridItem
+                    width="100%"
+                    height="100%"
+                    onClick={() => setExpand(!expand)}
+                  ></GridItem>
+                  <Box display={expand ? "contents" : "none"}>
+                    <GridItem />
+                    <Text>{plan.premium}</Text>
+                    <GridItem />
+                    <Text>{plan.deductibles[0].amount}</Text>
+                    <GridItem />
+                    <Text>{plan.moops[0].amount}</Text>
+                  </Box>
                   <GridItem />
-                  <Text>{plan.name}</Text>
                   <GridItem />
-                  <Text>{plan.premium}</Text>
+                  <Text>{plan.metal_level}</Text>
                   <GridItem />
-                  <Text>{plan.deductibles[0].amount}</Text>
+                  <Text>{plan.type}</Text>
                   <GridItem />
-                  <Text>{plan.moops[0].amount}</Text>
+                  <Text>{plan.id}</Text>
                 </Grid>
               );
             })}
