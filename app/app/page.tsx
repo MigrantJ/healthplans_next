94ffccf802098178c1015c2e222206ab3e64851c
Jim Grant <jimgrant@gmail.com>
2024-10-02 11:16:03 -0500

    more modern layout for splash page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -25,63 +25,79 @@ export default function IndexPage() {
 
   return (
     <Flex
-      flexDir="column"
-      maxWidth="600px"
-      margin="0 auto"
+      flexDir={"row"}
+      paddingX={"192px"}
+      paddingY={"96px"}
       backgroundColor={colorMode === "light" ? "white" : "black"}
-      boxShadow="6px 0 4px -4px black, -6px 0 4px -4px black"
-      alignItems="center"
-      padding="20px"
-      minHeight="100vh"
+      minHeight={"100vh"}
     >
-      <Heading size="xl">Find the Right Plan for You</Heading>
-      <Text marginX="20px" marginY="10px">
-        Through the Health Insurance Marketplace, you may qualify for
-        affordable, dependable healthcare plans. These plans include
-        prescription medications, medical appointments, urgent care, hospital
-        stays, and additional benefits.
-      </Text>
-      <Text marginX="20px" marginY="10px" fontWeight="bold">
-        Use this site to discover what plans you qualify for and see if
-        you&apos;re eligible for tax credits or cost reductions.
-      </Text>
       <Flex
-        bgColor={colorMode === "light" ? "main.100" : "main.800"}
-        padding="10px"
+        flexDir="column"
+        marginX="32px"
+        width="50%"
+        backgroundColor={colorMode === "light" ? "white" : "black"}
+        alignItems="left"
+      >
+        <Heading size="3xl">Find the Right Plan for You</Heading>
+        <Text marginTop="32px">
+          Through the Health Insurance Marketplace, you may qualify for
+          affordable, dependable healthcare plans. These plans include
+          prescription medications, medical appointments, urgent care, hospital
+          stays, and additional benefits.
+        </Text>
+        <Text marginY="10px" fontWeight="bold">
+          Use this site to discover what plans you qualify for and see if
+          you&apos;re eligible for tax credits or cost reductions.
+        </Text>
+        <Flex
+          bgColor={colorMode === "light" ? "main.100" : "main.800"}
+          padding="32px"
+          borderRadius="10px"
+          border="1px solid"
+          borderColor={colorMode === "light" ? "main.300" : "main.600"}
+          gap="10px"
+          direction="column"
+          marginTop="32px"
+          marginRight="48px"
+        >
+          <Heading size="xl" textAlign="left">
+            Start Here
+          </Heading>
+          <Text>
+            Enter your zip code, or press the <Icon as={RiMapPinLine} />
+            Geolocation button to find your zip code automatically.
+          </Text>
+          <Flex width="100%">
+            <LocationWidget
+              zipcode={location.data?.zipcode || ""}
+              isFetching={location.isFetching}
+            />
+            <Spacer />
+            <Link as={NextLink} href="/app/plans">
+              <Button
+                variant="sidebar"
+                isDisabled={!isFetched || !!alt_data}
+                size="lg"
+              >
+                See Plans
+              </Button>
+            </Link>
+          </Flex>
+        </Flex>
+        {alt_data && alt_data.type === "InvalidState" && (
+          <InvalidStateMessage {...alt_data} />
+        )}
+      </Flex>
+      <Flex
+        margin="0 auto"
+        backgroundColor={colorMode === "light" ? "white" : "black"}
+        width="50%"
         borderRadius="10px"
-        marginY="5px"
         border="1px solid"
         borderColor={colorMode === "light" ? "main.300" : "main.600"}
-        gap="10px"
-        direction="column"
+        marginX="32px"
       >
-        <Heading size="md" textAlign="left">
-          Start Here
-        </Heading>
-        <Text>
-          Enter your zip code, or press the <Icon as={RiMapPinLine} />
-          Geolocation button to find your zip code automatically.
-        </Text>
-        <Flex width="100%">
-          <LocationWidget
-            zipcode={location.data?.zipcode || ""}
-            isFetching={location.isFetching}
-          />
-          <Spacer />
-          <Link as={NextLink} href="/app/plans">
-            <Button
-              variant="sidebar"
-              isDisabled={!isFetched || !!alt_data}
-              size="lg"
-            >
-              See Plans
-            </Button>
-          </Link>
-        </Flex>
       </Flex>
-      {alt_data && alt_data.type === "InvalidState" && (
-        <InvalidStateMessage {...alt_data} />
-      )}
     </Flex>
   );
 }
